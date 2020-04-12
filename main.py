292a0db53aca7733f5144739f1536762075b4d6b
Sergio Delgado Quintero <sdelquin@gmail.com>
2020-04-12 18:32:51 +0100

    Add units

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ MB = 1024 * 1024
 GB = 1024 * MB
 
 
-@hug.get('/')
-def root():
+def get_stats():
     cpu_load = psutil.cpu_percent()
     cpu_freq = psutil.cpu_freq().current
     mem_available = psutil.virtual_memory().available / MB
     disk_available = psutil.disk_usage('/').free / GB
     return {
-        'cpu_load': cpu_load,
-        'cpu_freq': cpu_freq,
-        'mem_available': mem_available,
-        'disk_available': disk_available
+        'cpu_load': [cpu_load, '%'],
+        'cpu_freq': [cpu_freq, 'MHz'],
+        'mem_available': [mem_available, 'MB'],
+        'disk_available': [disk_available, 'GB']
     }
+
+
+@hug.get('/')
+def root():
+    return get_stats()
